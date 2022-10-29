bfa378a9d46d26d20dcee91ee380d9e056810c1a
Davide Wiest <davide.wiest2@gmail.com>
2022-10-30 00:13:39 +0200

    edit content, lay foundations of new projects scn

--- personal_website2/settings.py
+++ personal_website2/settings.py
@@ -71,14 +71,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'django_hosts',
+    # 'django_hosts',
 
     'ml',
     '_site'
 ]
 
 MIDDLEWARE = [
-    'django_hosts.middleware.HostsRequestMiddleware',
+    # 'django_hosts.middleware.HostsRequestMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -86,7 +86,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_hosts.middleware.HostsResponseMiddleware'
+    # 'django_hosts.middleware.HostsResponseMiddleware'
 ]
 
 ROOT_URLCONF = 'personal_website2.urls'
