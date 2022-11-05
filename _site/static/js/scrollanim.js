00dc47b09d9e0371167da7e6d929be41acb9cacb
Davide Wiest <davide.wiest2@gmail.com>
2022-11-05 21:23:34 +0100

    working on projects, hero page

--- _site/static/js/scrollanim.js
+++ _site/static/js/scrollanim.js
@@ -59,3 +59,34 @@ ScrollAnimImages2.forEach((image) => {
         }
     )
 })
+
+
+const ScrollAnimImages3 = document.querySelectorAll(".base_fade_in");
+
+
+ScrollAnimImages3.forEach((image) => {
+    const imgOffsetTop = image.offsetTop;
+    const imgHeight = image.offsetHeight;
+
+    const ScrollAnimImagesTimeline2 = new ScrollTimeline({
+        scrollOffsets: [
+            CSS.px(imgOffsetTop + imgHeight - window.innerHeight),
+            CSS.px(imgOffsetTop - window.innerHeight * 0.75),
+        ]
+    });
+
+    image.animate(
+        {
+            transform: [
+                "perspective(1200px) rotateX(20deg) rotate(0deg)",
+                "perspective(1200px) rotateX(0) rotate(-26deg) translateY(-15rem)"
+            ],
+            opacity: ["0.95", "1"]
+        },
+        {
+            duration: 1,
+            easing: "ease-in-out",
+            timeline: ScrollAnimImagesTimeline2
+        }
+    )
+})
