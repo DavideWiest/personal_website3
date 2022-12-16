a50200be2ed07694706f4fcb18c7ac7aef9b556e
Davide R. Wiest <davide.wiest2@gmail.com>
2022-12-16 17:12:39 +0100

    Update wsgi.py

--- personal_website2/wsgi.py
+++ personal_website2/wsgi.py
@@ -7,9 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
+
+# for production
+sys.path.append("/var/www/html/personal_website2")
+sys.path.append("var/www/html/personal_website2/personal_website2")
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'personal_website2.settings')
 
