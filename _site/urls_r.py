0d30ae05ea79dac20eba2f295e1626bdfce034ab
Davide Wiest <davide.wiest2@gmail.com>
2023-08-29 19:28:10 +0000

    fix unmerged github files

--- _site/urls_r.py
+++ _site/urls_r.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from django.urls import path
 from django.http import FileResponse
 from django.http import HttpResponseNotFound
@@ -13,20 +12,4 @@ def other(request, resource_title):
 
 urlpatterns = [
     path("<str:resource_title>", other)
-=======
-from django.urls import path
-from django.http import FileResponse
-from django.http import HttpResponseNotFound
-
-from .base import choose_lang
-
-def other(request, resource_title):
-    try:
-        return FileResponse(open(f'_site/static/resources/{resource_title}', 'rb'), content_type='application/pdf')
-    except:
-        return HttpResponseNotFound("This file does not exist.")
-
-urlpatterns = [
-    path("<str:resource_title>", other)
->>>>>>> 8f772811bbf909d83ef59f27f656476037510343
 ]
